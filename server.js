c6dba99cee93b1f95f614261f0e19eb0c54215f0
Jim Ritchey <jim0ritchey@gmail.com>
2018-12-20 19:45:01 -0500

    Added user storage variables and set up express to handle data parsing

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var reservations = {};
+var waiting = {};
+
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
