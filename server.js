6a2d408d80b9e6bfee89fe9b13fd466cfab0497a
Spiderman <santiagosepulveda@me.com>
2018-12-20 19:47:29 -0500

    server file modified

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
-// Listener
+// Routes
+// ===========================================================
+app.get("/", function(req, res) {
+    res.send("Welcome to the Star Wars Page!");
+  });
+
+  // Listener
 // ===========================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
+
