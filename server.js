fabce0bca8fd8f99658bdc51aca5cfbfca92b574
Jim Ritchey <jim0ritchey@gmail.com>
2018-12-20 20:16:01 -0500

    Added get calls for the api and view tables page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 // ===========================================================
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
@@ -9,8 +10,21 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var reservations = {};
-var waiting = {};
+var reservations = [];
+var waiting = [];
+
+// Get calls
+app.get("/api/tables", function(req, res) {
+  return res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waiting);
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+})
 
 // Listener
 // ===========================================================
